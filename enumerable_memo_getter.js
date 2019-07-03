499aebcafb3ae953548f4f3a76d3420f21ce9e75
2tankov2 <domino-82@mail.ru>
2019-07-03 19:21:56 +0300

    add memo&getter

---
+++ enumerable_memo_getter.js
@@ -0,0 +1,78 @@
+class Enumerable {
+    constructor(collection, operations) {
+      this.collection = collection;
+      this.operations = operations || [];
+    }
+  
+    build(fn) {
+      return new Enumerable(this.collection.slice(), this.operations.concat(fn));
+    }
+  
+    select(fn) {
+      return this.build(coll => coll.map(fn));
+    }
+  
+    orderBy(fn, direction = 'asc') {
+      const comparator = (a, b) => {
+        const a1 = fn(a);
+        const b1 = fn(b);
+  
+        const compareResult = direction === 'asc' ? 1 : -1;
+  
+        if (a1 > b1) {
+          return compareResult;
+        }
+  
+        if (a1 < b1) {
+          return -compareResult;
+        }
+  
+        return 0;
+      };
+      return this.build(coll => coll.sort(comparator));
+    }
+  
+    where(fn) {
+      return this.build(coll => coll.filter(fn));
+    }
+  
+    // BEGIN (write your solution here)
+    memoToArray() {
+      if (!this.memo) {
+        const newCol = this.collection.slice();
+        this.memo = this.operations.reduce((acc, func) => func(acc), newCol);
+      }
+      console.log(this.memo);
+      return this.memo;
+    }
+  
+    toArray() {
+      return this.memoToArray().slice();
+    }
+  
+    get length() {
+      return this.memoToArray().length;
+    }
+    // END
+  }
+  
+  /* 
+  getProcessedCollection() {
+    if (!this.memo) {
+      this.memo = this.operations.reduce((acc, func) => func(acc), this.collection);
+    }
+
+    return this.memo;
+  }
+
+  get length() {
+    return this.getProcessedCollection().length;
+  }
+
+  toArray() {
+    return this.getProcessedCollection().slice();
+  }
+  */
+
+  export default Enumerable;
+  
